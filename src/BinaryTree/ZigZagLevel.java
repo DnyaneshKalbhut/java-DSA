8fe34483a3b9f3acf05f5c3aa0d019cdda72cea5
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2023-10-18 11:40:23 +0530

    tree

---
+++ src/BinaryTree/ZigZagLevel.java
@@ -0,0 +1,66 @@
+package BinaryTree;
+
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ZigZagLevel {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> zigzaglevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean reverse = false;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                if (!reverse) {
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.addLast(currentNode.left);
+                    }
+                } else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
+
