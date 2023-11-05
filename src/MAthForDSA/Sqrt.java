9fe31ea6023a91606be863e9a030f8c59e965da1
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2023-11-05 23:36:22 +0530

    Maths

---
+++ src/MAthForDSA/Sqrt.java
@@ -0,0 +1,37 @@
+package MAthForDSA;
+
+public class Sqrt {
+    public static void main(String[] args) {
+int n = 40;
+int p =3;
+        System.out.println(sqrt(n,p));
+    }
+
+    public static double sqrt(int n , int p){
+
+        int start = 0;
+        int end = n;
+        double root =0.0;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if (mid*mid==n){
+                return mid;
+            }
+            if (mid*mid>n){
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+        }
+
+        double inc = 0.1;
+        for (int i = 0; i <p ; i++) {
+            while (root*root<n){
+                root+=inc;
+            }
+            root-=inc;
+            inc/=10;
+        }
+        return root;
+    }
+}
