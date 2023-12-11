6c0cdc04a11ced0265dad54ca787a09411dfe34c
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2023-12-11 22:56:07 +0530

    Array Questions

---
+++ src/KunalDSAsheet/ArrayQuestions/Concatenation.java
@@ -0,0 +1,13 @@
+package KunalDSAsheet.ArrayQuestions;
+
+public class Concatenation {
+    public int[] getConcatenation(int[] nums) {
+        int []ans = new int[2*nums.length];
+        int n = nums.length;
+        for(int i = 0 ; i<nums.length ; i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
