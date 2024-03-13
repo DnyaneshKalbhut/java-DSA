eea67159071a00b47a57b84fba0bbf396f4b611a
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-03-14 00:50:25 +0530

    Dsa Questions

--- src/KunalDSAsheet/Spcl/Revision2.java
+++ src/KunalDSAsheet/Spcl/Revision2.java
@@ -1,5 +1,6 @@
 package KunalDSAsheet.Spcl;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,12 +8,64 @@ public class Revision2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a num to reverse it ");
-        int val = sc.nextInt();
+        System.out.println("Enter a number");
+        int val =sc.nextInt();
 
-        int reverse = reverNum(val);
-        System.out.println(reverse);
+        if (isArmstring(val)){
+            System.out.println("yes");
+        }else {
+
+            System.out.println("no");
+        }
+
     }
+
+
+    private static boolean isArmstring(int num){
+        int result =0;
+        int originalNum=num;
+        int count =0;
+        int rem=0;
+
+        while (originalNum!=0){
+            originalNum /=10;
+            ++count;
+        }
+
+        originalNum=num;
+
+        while (originalNum!=0){
+            rem = originalNum % 10;
+            result+=Math.pow(rem,count);
+            originalNum/=10;
+        }
+         return result==num;
+    }
+
+
+    private static void sortArr(int[] arr) {
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j <arr.length ; j++) {
+                if (arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+
+    }
+
+    private static int maxVal(int[] arr) {
+        int max =0;
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+
 
     private static int reverNum(int val) {
         int reverse =0;
